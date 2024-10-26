59924356521239bb6b56d6295f380eda3fb0e5e2
aayush-navsariwala <aayush.nav@gmail.com>
2024-10-27 00:55:13 +0200

    Commit 23 - Test 23

---
+++ app/src/test/java/com/theateam/vitaflex/SharedPreferencesTest.kt
@@ -0,0 +1,26 @@
+package com.theateam.vitaflex
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SharedPreferencesTest {
+    @Test
+    fun testSaveAndRetrieveData() {
+        // Obtain the application context
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        // Access SharedPreferences in a private mode for testing
+        val sharedPreferences = context.getSharedPreferences("TestPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        // Put a string value in SharedPreferences and commit
+        editor.putString("test_key", "test_value")
+        editor.apply()
+
+        // Retrieve the value and validate
+        val savedValue = sharedPreferences.getString("test_key", null)
+        assertEquals("The retrieved value should match the saved value", "test_value", savedValue)
+    }
+}
