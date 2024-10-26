e977cee117108c7e0630936ff01595577a673892
aayush-navsariwala <aayush.nav@gmail.com>
2024-10-27 00:08:10 +0200

    Commit 20 - Test 20

--- app/src/test/java/com/theateam/vitaflex/NetworkConnectivityTest.kt
+++ app/src/test/java/com/theateam/vitaflex/NetworkConnectivityTest.kt
@@ -5,18 +5,21 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import androidx.test.core.app.ApplicationProvider
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 class NetworkConnectivityTest {
+    private lateinit var connectivityManager: ConnectivityManager
+
+    @Before
+    fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
+
     @Test
     fun testNetworkConnectivity() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val network = connectivityManager.activeNetwork
-        val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
-        val isConnected = networkCapabilities != null && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-
-        // This assumes you are running the test in an online environment
-        assertTrue("The device should have internet connectivity", isConnected)
+        val activeNetwork = connectivityManager.activeNetworkInfo
+        assertTrue("Network is not connected", activeNetwork != null && activeNetwork.isConnected)
     }
 }
