12ba11eec36cfa71ac403891ced9f77c8bcb62cb
aayush-navsariwala <aayush.nav@gmail.com>
2024-10-27 01:01:27 +0200

    Commit 24 - Test 24

--- app/src/androidTest/java/com/theateam/vitaflex/SharedPreferencesTest.kt
+++ app/src/androidTest/java/com/theateam/vitaflex/SharedPreferencesTest.kt
@@ -2,9 +2,12 @@ package com.theateam.vitaflex
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class SharedPreferencesTest {
     @Test
     fun testSaveAndRetrieveData() {
