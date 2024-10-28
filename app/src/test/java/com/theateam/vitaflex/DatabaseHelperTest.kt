3b9e9beb5b3fab0e831dc0a6fe237789aa5d8fee
aayush-navsariwala <aayush.nav@gmail.com>
2024-10-28 07:16:04 +0200

    Commit 26 - Final Commit

--- app/src/test/java/com/theateam/vitaflex/DatabaseHelperTest.kt
+++ app/src/test/java/com/theateam/vitaflex/DatabaseHelperTest.kt
@@ -2,13 +2,10 @@ package com.theateam.vitaflex
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import java.io.File
 
 //@RunWith(AndroidJUnit4::class)
 class DatabaseHelperTest {
